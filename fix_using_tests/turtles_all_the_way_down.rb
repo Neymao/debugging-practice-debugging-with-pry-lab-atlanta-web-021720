7affe1bf3612d32ab3c820f4c15e03f2a9acebf1
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-02-12 14:15:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -1,17 +1,24 @@
+require 'pry'
 
 def turtles
     [
       {name: "Donatello", weapon: "bo-staff", traits: ["serious", "leader"]},
-      {name: "Michaelangelo", weapon: "nunchuks", traits: "party dude"},
+      {name: "Michaelangelo", weapon: "nunchuks", traits: ["party dude"]},
       {name: "Raphael", weapon: "sais", traits: ["cool", "rude"]},
       {name: "Leonardo", weapon: "katanas", traits: ["loves being a turtle"]}
     ]
 end
 
 def turtle_traits(turtles)
-  turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
+  
+  turtles.each do |turtles|
+    if turtles[traits:] = traits
+      return turtles[traits:]
+    
     end
   end
 end
+
+
+
+
