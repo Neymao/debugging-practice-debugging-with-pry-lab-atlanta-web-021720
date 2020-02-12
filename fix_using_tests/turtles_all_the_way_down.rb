74843bf9c9224ccfa279011ece035f76d4f86f0e
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-02-12 14:41:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/turtles_all_the_way_down.rb
+++ fix_using_tests/turtles_all_the_way_down.rb
@@ -9,16 +9,3 @@ def turtles
     ]
 end
 
-
-  
-  
-  
-def turtle_traits(turtles)
-  turtles.map do |turtle|
-    turtle[:traits].each do |trait|
-      trait
-    end
-  end
-end
-  turtles.map{ |turtle| turtle[:traits] }
-end 
